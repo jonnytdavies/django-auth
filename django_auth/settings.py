c989b7ef9f2d5687613a1e5c349f3f94460fe889
Jonny Thomas-Davies <jonnytdavies@aol.com>
2020-04-21 14:09:01 +0000

    updated settings again

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -136,6 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ["EMAIL_ADDRESS"]
